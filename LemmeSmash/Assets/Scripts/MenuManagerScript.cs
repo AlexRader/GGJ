35b62f4e9a6dea76aa6a66509844ea7607cff042
WesleyWeitzman <wesley.weitzman@mymail.champlain.edu>
2018-01-27 19:45:25 -0500

    Interscene Sounds Added -tracks for various levels
    Menu Screen Developed
    -SFX
    -buttons & navigation

---
+++ LemmeSmash/Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManagerScript : MonoBehaviour {
+
+	public GameObject faderFab;
+	public GameObject mainMenu;
+	public GameObject creditScreen;
+	public GameObject controlPage;
+
+	float delayTransition = 0.7f;
+	float delayConstant = 0.7f;
+	bool countdown = false;
+	bool goToMain = false;
+	bool goToCredits = false;
+	bool goToControls = false;
+	bool startGame = false;
+
+	public void Transition(){
+		Instantiate (faderFab);
+	}
+
+	void Update(){
+		if (countdown) {
+			delayTransition -= Time.deltaTime;
+			if (delayTransition <= 0) {
+				if (goToMain) {
+					MenuOn ();
+				} else if (goToCredits) {
+					CreditsOn ();
+				} else if (goToControls) {
+					ControlsOn ();
+				} else if (startGame) {
+					Debug.Log ("game time binches");
+				}
+				countdown = false;
+				delayTransition = delayConstant;
+			}
+		}
+	}
+
+	public void PlayGame(){
+		Transition ();
+		countdown = true;
+		startGame = true;
+	}
+
+	public void GoToControls(){
+		Transition ();
+		countdown = true;
+		goToControls = true;
+	}
+
+	public void GoTOCredits(){
+		Transition ();
+		countdown = true;
+		goToCredits = true;
+	}
+
+	public void GoToMenu(){
+		Transition ();
+		countdown = true;
+		goToMain = true;
+	}
+
+	void MenuOn(){
+		goToMain = false;
+		mainMenu.SetActive (true);
+		creditScreen.SetActive (false);
+		controlPage.SetActive (false);
+	}
+
+	void CreditsOn (){
+		goToCredits = false;
+		mainMenu.SetActive (false);
+		creditScreen.SetActive (true);
+		controlPage.SetActive (false);
+	}
+
+	void ControlsOn(){
+		goToControls = false;
+		mainMenu.SetActive (false);
+		creditScreen.SetActive (false);
+		controlPage.SetActive (true);
+	}
+
+}
